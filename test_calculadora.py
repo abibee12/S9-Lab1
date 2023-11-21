5fd8b0d0f3a40c417f64f17988da95c8fad81067
abibee12 <Carlaamc02@gmail.com>
2023-11-21 21:58:39 +0100

    agrego la calculadora con todas las operaciones

---
+++ test_calculadora.py
@@ -0,0 +1,31 @@
+import unittest
+from calculadora import sumar, restar, multiplicar, dividir
+
+
+class TestOperacionesMatematicas(unittest.TestCase):
+    def test_sumar(self):
+        self.assertEqual(sumar(3, 2), 5)
+        self.assertEqual(sumar(-1, 1), 0)
+        self.assertEqual(sumar(-1, -1), -2)
+
+    def test_restar(self):
+        self.assertEqual(restar(5, 3), 2)
+        self.assertEqual(restar(-1, 1), -2)
+        self.assertEqual(restar(-1, -1), 0)
+
+    def test_multiplicar(self):
+        self.assertEqual(multiplicar(3, 2), 6)
+        self.assertEqual(multiplicar(-1, 1), -1)
+        self.assertEqual(multiplicar(-1, -1), 1)
+
+    def test_dividir(self):
+        self.assertEqual(dividir(6, 3), 2)
+        self.assertEqual(dividir(-4, 2), -2)
+
+        # Asegurar que dividir por cero produce un ValueError
+        with self.assertRaises(ValueError):
+            dividir(5, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
